16bf91ed861541bc5f36cede7062ff48f7b2659e
kk <kk@youzan.com>
2020-03-15 09:46:59 +0800

    update bubble sort

---
+++ src/bubbleSort.js
@@ -0,0 +1,23 @@
+/**
+ * 冒泡排序
+ * @param {Array} arr 待排序数组
+ */
+function bubbleSort(arr) {
+  const len = arr.length;
+
+  for (let i = 0; i < len; i++) {
+    let flag = false;
+    for (let j = 0; j < len - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        flag = true;
+      }
+    }
+    if (!flag) break;
+  }
+  return arr;
+}
+
+module.exports = bubbleSort;
