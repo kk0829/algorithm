859f491daadc86085e3cab7ac29faf95cbb6e890
kk <244098979@qq.com>
2022-02-22 08:48:03 +0800

    update

--- java/src/BubbleSort.java
+++ java/src/BubbleSort.java
@@ -1,10 +1,26 @@
 public class BubbleSort {
 
     public static void main(String[] args) {
-
+        int[] arr = new int[]{6, 4, 3, 8, 1};
+        int[] newArr = new BubbleSort().bubbleSort(arr);
+        System.out.println(newArr);
     }
 
-    public int bubbleSort(int[] arr) {
+    public int[] bubbleSort(int[] arr) {
+        int len = arr.length;
+        if (len <= 1) {
+            return arr;
+        }
 
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+        return arr;
     }
 }
